813f58fe7af9b341538664020bc539c8bfebc728
Sasha R <al.d.rykov@gmail.com>
2025-06-09 18:33:05 +0200

    made async

--- sound.py
+++ sound.py
@@ -1,40 +1,46 @@
 import time
+import asyncio
 
-import queue
 import sounddevice as sd
 
 from config import settings
 
 
-audio_queue = queue.Queue()
+class AudioProcessor:
+    def __init__(self):
+        self.loop: asyncio.AbstractEventLoop | None = None
+        self.queue: asyncio.Queue[bytes] = asyncio.Queue(maxsize=100)
 
+    def _callback(self, indata, frames, time, status):
+        if status:
+            print(f"[!] Status: {status}")
+        data = indata.copy().tobytes()
+        self.loop.call_soon_threadsafe(self.queue.put_nowait, data)
 
-def callback(indata, frames, time, status):
-    if status:
-        print(f"[!] Status: {status}")
-    audio_queue.put(indata.copy())
+    async def consume(self):
+        while True:
+            chunk = await self.queue.get()
+            print(f"Got audio chunk: {len(chunk)} bytes")
 
+            # Placeholder for processing
+            await asyncio.sleep(0)
 
-def main():
-    print("Starting audio stream. Press Ctrl+C to stop.")
-
-    for device_idx in range(settings.input_n_devices):
+    async def run(self):
+        print("Starting audio recording. Press Ctrl+C to stop.")
+        self.loop = asyncio.get_running_loop()
         with sd.InputStream(
-            samplerate=settings.input_samplerate[device_idx],
-            blocksize=int(settings.input_samplerate[device_idx] * settings.chunk_size_seconds),
-            channels=settings.input_n_channels[device_idx],
-            device=settings.input_sd_device_ids[device_idx],
-            dtype=settings.dtype,
-            callback=callback
+                samplerate=settings.input_samplerate[0],
+                blocksize=int(settings.input_samplerate[0] * settings.chunk_size_seconds),
+                channels=settings.input_n_channels[0],
+                device=settings.input_sd_device_ids[0],
+                dtype=settings.dtype,
+                callback=self._callback
         ):
-            try:
-                while True:
-                    chunk = audio_queue.get()
-                    print(f"Received audio chunk with shape: {chunk.shape}, dtype: {chunk.dtype}, time: {time.time()}")
-
-            except KeyboardInterrupt:
-                print("\nStopped by user.")
+            await self.consume()
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        asyncio.run(AudioProcessor().run())
+    except KeyboardInterrupt:
+        print("\nStopped.")
